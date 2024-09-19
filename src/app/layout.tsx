635f23408dd9aac65353eef925998c019fcec953
moisur <contact@lecoledes1.com>
2024-09-19 19:37:49 +0200

    mise a jour

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'CTRL by Gicé',
   description: 'Révolutionnez vos tweets avec CTRL, le SaaS ultime pour dominer Twitter !',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="fr">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
